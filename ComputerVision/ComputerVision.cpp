850b86a090c0e935e49f28a37abcdac6569d7c2e
yabushi <hdecogtrexi557960@gmail.com>
2019-08-20 23:37:59 +0900

    [Update] Comments

--- ComputerVision/ComputerVision.cpp
+++ ComputerVision/ComputerVision.cpp
@@ -24,17 +24,17 @@ int main()
 		cvtColor(frame, frame, CV_BGR2GRAY);
 
 		/*
-		Binarization using Otsu's method
-		THRESH_BINARY: If the threshold is exceeded, the maximum value is set, otherwise it is 0.
-		*/
+		 * Binarization using Otsu's method
+		 * THRESH_BINARY: If the threshold is exceeded, the maximum value is set, otherwise it is 0.
+		 */
 		threshold(frame, frame, 0, 255, THRESH_BINARY | THRESH_OTSU);
 
 		/*
-		Morphological operations (Opening)
-		The fourth argument Mat() is structure element (kernel) that the default size is 3x3.
-		The fifth argument Point(-1, -1), the center of the kernel is used as the anchor.
-		The sixth argument is the number of reductions and expansions.
-		*/
+		 * Morphological operations (Opening)
+		 * The fourth argument Mat() is structure element (kernel) that the default size is 3x3.
+		 * The fifth argument Point(-1, -1), the center of the kernel is used as the anchor.
+		 * The sixth argument is the number of reductions and expansions.
+		 */
 		morphologyEx(frame, frame, MORPH_OPEN, Mat(), Point(-1, -1), 1);
 
 		// TODO: Detect contour
